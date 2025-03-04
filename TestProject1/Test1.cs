79741a580128a1eb7152ca26b3700583706c2894
fiechdus <fiechdus@students.zhaw.ch>
2025-03-04 18:47:31 +0100

    initial commit

---
+++ TestProject1/Test1.cs
@@ -0,0 +1,28 @@
+﻿using WebApplication1;
+
+namespace TestProject1;
+
+[TestClass]
+public sealed class Test1
+{
+    private ToDoController _controller;
+    
+    [TestInitialize]
+    public void Initialize()
+    {
+        _controller = new ToDoController();
+    }
+    
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.AreEqual(true, true);
+    }
+    
+    [TestMethod]
+    public void TestMethodApiToDo()
+    {
+        // TODO: Fix this error
+        Assert.AreEqual(_controller.getToDos(), new List<ToDo>());
+    }
+}
---
+++ WebApplication1/ToDoController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1;
+
+[ApiController, Route("api/[controller]")]
+public class ToDoController : ControllerBase
+{
+    public ToDoController()
+    {
+        
+    }
+
+
+    [HttpGet, Route("all")]
+    public IActionResult getToDos()
+    {
+
+        return Ok(new List<ToDo>());
+    }
+}
+
+
+public class ToDo
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+}
